2b73daa74d113f6314c5442293f8a02620393edb
Salah Dirchaoui <salahdirchaoui@gmail.com>
2020-11-12 15:48:39 +0000

    add a course to firestore

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
+
+// to add a course to firestore
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const now = new Date();
+  const course = {
+    title: form.course.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection('courses')
+    .add(course)
+    .then((res) => console.log(res, 'course added'))
+    .catch((err) => console.error(err));
+});
 
 // to retreive data from firestore to UI
 addCourse = (course) => {
@@ -10,7 +25,7 @@ addCourse = (course) => {
   `;
   list.innerHTML += html;
 };
-
+// to get data from firstore
 db.collection('courses')
   .get()
   .then((res) =>
